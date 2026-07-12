53d691d2cd66452fa77fee00d0f4307920e039cb
Carmen Alvarez <carmen@rmen.ca>
2026-07-12 22:50:14 +0200

    ios: Setup XCode.
    - Add a reference to the cobol source set.
    - Add a build rule to transpile the cobol files.
    - Add a bridging header (not sure if it's needed).
    - Configure linking with -lxml2.

---
+++ iosApp/iosApp/iosApp-BridgingHeader.h
@@ -0,0 +1,12 @@
+//
+//  iosApp-BridgingHeader.h
+//  iosApp
+//
+//  Created by Carmen Alvarez on 12/07/2026.
+//
+
+#ifndef iosApp_BridgingHeader_h
+#define iosApp_BridgingHeader_h
+
+
+#endif /* iosApp_BridgingHeader_h */
