1e78349e02282d293122820e2e144c8c0150fb34
Carmen Alvarez <carmen@rmen.ca>
2026-07-14 13:36:52 +0200

    Make the main view scrollable.

--- shared/src/commonMain/kotlin/ca/rmen/tipcalculator/App.kt
+++ shared/src/commonMain/kotlin/ca/rmen/tipcalculator/App.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
@@ -55,7 +57,7 @@ fun App(
         val tipReportContent by viewModel.tipReportContent.collectAsState()
         Column(
             modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding().fillMaxSize(),
+                .safeContentPadding().fillMaxSize().verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             TextField(
