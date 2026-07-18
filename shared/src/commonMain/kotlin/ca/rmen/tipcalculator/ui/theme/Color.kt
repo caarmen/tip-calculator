364d3af992551bda5c25e127810b96ffedc320a8
Carmen Alvarez <carmen@rmen.ca>
2026-07-19 00:57:10 +0200

    Use a green theme.

--- shared/src/commonMain/kotlin/ca/rmen/tipcalculator/ui/theme/Color.kt
+++ shared/src/commonMain/kotlin/ca/rmen/tipcalculator/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package ca.rmen.tipcalculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val formBackgroundColor = Color(red = 0, green = 21, blue = 152)
-val formLabelTextColor = Color(red = 255, green = 255, blue = 0)
-val formInputTextColor = Color(red = 255, green = 255, blue = 255)
-val formDisabledTextColor = Color(red = 128, green = 128, blue = 128)
+val formBackgroundColor = Color(red = 0, green = 32, blue = 0)
+val formLabelTextColor = Color(red = 0, green = 255, blue = 0)
+val formInputTextColor = Color(red = 0, green = 255, blue = 0)
+val formDisabledTextColor = Color(red = 0, green = 64, blue = 0)
