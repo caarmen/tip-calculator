aa43205318fd02b61a0281502c532e91074721c3
Carmen Alvarez <carmen@rmen.ca>
2026-07-18 22:53:58 +0200

    Remove the preview from MainActivity
    We have preview already on TipScreen.

--- androidApp/src/main/kotlin/ca/rmen/tipcalculator/MainActivity.kt
+++ androidApp/src/main/kotlin/ca/rmen/tipcalculator/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import ca.rmen.tipcalculator.domain.calculator.AndroidTipCalculator
@@ -36,10 +34,4 @@ class MainActivity : ComponentActivity() {
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
