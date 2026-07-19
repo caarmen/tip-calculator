e60b4ee26c3133ed23d4205c3015ae9b4710fba6
Carmen Alvarez <carmen@rmen.ca>
2026-07-19 16:45:52 +0200

    feat: Add the dec term font.

--- shared/src/commonMain/kotlin/ca/rmen/tipcalculator/ui/theme/Theme.kt
+++ shared/src/commonMain/kotlin/ca/rmen/tipcalculator/ui/theme/Theme.kt
@@ -1,34 +1,98 @@
 package ca.rmen.tipcalculator.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import org.jetbrains.compose.resources.Font
+import tipcalculator.shared.generated.resources.Res
+import tipcalculator.shared.generated.resources.decterm
 
-private fun appTypography(fontFamily: FontFamily) = Typography().run {
-    Typography(
-        displayLarge = displayLarge.copy(fontFamily = fontFamily),
-        displayMedium = displayMedium.copy(fontFamily = fontFamily),
-        displaySmall = displaySmall.copy(fontFamily = fontFamily),
-        headlineLarge = headlineLarge.copy(fontFamily = fontFamily),
-        headlineMedium = headlineMedium.copy(fontFamily = fontFamily),
-        headlineSmall = headlineSmall.copy(fontFamily = fontFamily),
-        titleLarge = titleLarge.copy(fontFamily = fontFamily),
-        titleMedium = titleMedium.copy(fontFamily = fontFamily),
-        titleSmall = titleSmall.copy(fontFamily = fontFamily),
-        bodyLarge = bodyLarge.copy(fontFamily = fontFamily),
-        bodyMedium = bodyMedium.copy(fontFamily = fontFamily),
-        bodySmall = bodySmall.copy(fontFamily = fontFamily),
-        labelLarge = labelLarge.copy(fontFamily = fontFamily),
-        labelMedium = labelMedium.copy(fontFamily = fontFamily),
-        labelSmall = labelSmall.copy(fontFamily = fontFamily),
-    )
-}
+@Composable
+private fun appTypography(
+    fontFamily: FontFamily,
+    fontSizeMultiplier: Double = 1.0,
+): Typography =
+    with(MaterialTheme.typography) {
+        copy(
+            displayLarge = displayLarge.copy(
+                fontFamily = fontFamily,
+                fontSize = displayLarge.fontSize * fontSizeMultiplier
+            ),
+            displayMedium = displayMedium.copy(
+                fontFamily = fontFamily,
+                fontSize = displayMedium.fontSize * fontSizeMultiplier
+            ),
+            displaySmall = displaySmall.copy(
+                fontFamily = fontFamily,
+                fontSize = displaySmall.fontSize * fontSizeMultiplier
+            ),
+            headlineLarge = headlineLarge.copy(
+                fontFamily = fontFamily,
+                fontSize = headlineLarge.fontSize * fontSizeMultiplier
+            ),
+            headlineMedium = headlineMedium.copy(
+                fontFamily = fontFamily,
+                fontSize = headlineMedium.fontSize * fontSizeMultiplier
+            ),
+            headlineSmall = headlineSmall.copy(
+                fontFamily = fontFamily,
+                fontSize = headlineSmall.fontSize * fontSizeMultiplier
+            ),
+            titleLarge = titleLarge.copy(
+                fontFamily = fontFamily,
+                fontSize = titleLarge.fontSize * fontSizeMultiplier
+            ),
+            titleMedium = titleMedium.copy(
+                fontFamily = fontFamily,
+                fontSize = titleMedium.fontSize * fontSizeMultiplier
+            ),
+            titleSmall = titleSmall.copy(
+                fontFamily = fontFamily,
+                fontSize = titleSmall.fontSize * fontSizeMultiplier
+            ),
+            bodyLarge = bodyLarge.copy(
+                fontFamily = fontFamily,
+                fontSize = bodyLarge.fontSize * fontSizeMultiplier
+            ),
+            bodyMedium = bodyMedium.copy(
+                fontFamily = fontFamily,
+                fontSize = bodyMedium.fontSize * fontSizeMultiplier
+            ),
+            bodySmall = bodySmall.copy(
+                fontFamily = fontFamily,
+                fontSize = bodySmall.fontSize * fontSizeMultiplier
+            ),
+            labelLarge = labelLarge.copy(
+                fontFamily = fontFamily,
+                fontSize = labelLarge.fontSize * fontSizeMultiplier
+            ),
+            labelMedium = labelMedium.copy(
+                fontFamily = fontFamily,
+                fontSize = labelMedium.fontSize * fontSizeMultiplier
+            ),
+            labelSmall = labelSmall.copy(
+                fontFamily = fontFamily,
+                fontSize = labelSmall.fontSize * fontSizeMultiplier
+            ),
+        )
+    }
 
 @Composable
 fun AppTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        typography = appTypography(FontFamily.Monospace),
+        typography = appTypography(
+            // https://www.dafont.com/dec-terminal-modern.font
+            fontFamily = FontFamily(
+                Font(Res.font.decterm, FontWeight.Black)
+            ),
+            fontSizeMultiplier = 1.5,
+        ),
+        content = content,
+    )
+    MaterialTheme(
+        typography = appTypography(appFontFamily),
         content = content,
     )
 }
