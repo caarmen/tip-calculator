84e4024b956e8b8213863ca3b2a155d577c88118
Carmen Alvarez <carmen@rmen.ca>
2026-07-21 23:08:44 +0200

    Further reduce the number of columns in the report.

--- shared/src/commonMain/cobol/tipreport.cob
+++ shared/src/commonMain/cobol/tipreport.cob
@@ -43,6 +43,9 @@
        local-storage section.
       *>-------------------------------------------------------------
        01 ls-date pic 9(8).
+       01 ls-time pic 9(8).
+       01 ls-formatted-time pic x(5).
+
        01 ls-total-with-tip-amount comp-2.
 
 
@@ -62,70 +65,72 @@
 
        01 type is report heading.
            05 line number plus 1.
-               10 col 1 source c-stars pic x(38).
+               10 col 1 source c-stars pic x(32).
+           05 line number plus 1.
+               10 col 1 value 'TIPPING REPORT'.
+               10 col 25 value 'PAGE:'.
+               10 col 30 source page-counter pic zz9.
            05 line number plus 1.
                10 col 1 source ls-date pic 9999/99/99.
-               10 col 13 value 'Tipping summary'.
-               10 col 31 value 'Page:'.
-               10 col 37 source page-counter pic z9.
+               10 col 28 source ls-formatted-time pic x(5).
            05 line number plus 1.
-               10 col 1 source c-stars pic x(38).
+               10 col 1 source c-stars pic x(32).
 
        01 bill-detail type is detail.
            05 line number plus 2.
-               10 col 3 value 'BILL DETAILS'.
+               10 col 1 value 'BILL DETAILS'.
            05 line number plus 1.
-               10 col 3 source c-double-dashes pic x(12).
+               10 col 1 source c-double-dashes pic x(12).
            05 line number plus 1.
-               10 col 3 value 'SUBTOTAL:'.
-               10 col 26 source f-pretax-amount pic zzzzz9.99.
+               10 col 1 value 'SUBTOTAL:'.
+               10 col 24 source f-pretax-amount pic zzzzz9.99.
            05 line number plus 1.
-               10 col 3 value 'TAX AMOUNT:'.
-               10 col 26 source f-tax-amount pic zzzzz9.99.
+               10 col 1 value 'TAX AMOUNT:'.
+               10 col 24 source f-tax-amount pic zzzzz9.99.
            05 line number plus 1.
-               10 col 3 source c-dashes pic x(32).
+               10 col 1 source c-dashes pic x(32).
            05 line number plus 1.
-               10 col 3 value 'BILL TOTAL:'.
-               10 col 26 source f-amount-with-tax pic zzzzz9.99.
+               10 col 1 value 'BILL TOTAL:'.
+               10 col 24 source f-amount-with-tax pic zzzzz9.99.
 
        01 tip-detail type is detail.
            05 line number plus 3.
-               10 col 3 value 'TIPPING'.
+               10 col 1 value 'TIPPING'.
            05 line number plus 1.
-               10 col 3 source c-double-dashes pic x(7).
+               10 col 1 source c-double-dashes pic x(7).
            05 line number plus 1.
-               10 col 3 value 'SERVICE QUALITY:'.
-               10 col 25 source f-service-level pic x(10).
+               10 col 1 value 'SERVICE QUALITY:'.
+               10 col 23 source f-service-level pic x(10).
            05 line number plus 1.
-               10 col 3 value 'TIP PERCENTAGE:'.
-               10 col 31 source f-tip-percentage pic z99.
-               10 col 34 value '%'.
+               10 col 1 value 'TIP PERCENTAGE:'.
+               10 col 29 source f-tip-percentage pic z99.
+               10 col 32 value '%'.
            05 line number plus 1.
-               10 col 3 value 'NUMBER OF PEOPLE:'.
-               10 col 32 source f-number-customers pic zz9.
+               10 col 1 value 'NUMBER OF PEOPLE:'.
+               10 col 30 source f-number-customers pic zz9.
 
        01 total-detail type is detail.
            05 line number plus 3.
-               10 col 3 value 'TOTALS'.
+               10 col 1 value 'TOTALS'.
            05 line number plus 1.
-               10 col 3 source c-double-dashes pic x(6).
+               10 col 1 source c-double-dashes pic x(6).
            05 line number plus 1.
-               10 col 3 value 'TOTAL TIP:'.
-               10 col 26 source f-total-tip-txt pic zzzzz9.99.
+               10 col 1 value 'TOTAL TIP:'.
+               10 col 24 source f-total-tip-txt pic zzzzz9.99.
            05 line number plus 1.
-               10 col 3 value 'TIP PER PERSON:'.
-               10 col 26 source f-tip-per-customer-txt pic zzzzz9.99.
+               10 col 1 value 'TIP PER PERSON:'.
+               10 col 24 source f-tip-per-customer-txt pic zzzzz9.99.
            05 line number plus 2.
-               10 col 3 value 'GRAND TOTAL (INC. TIP):'.
-               10 col 26 source f-total-with-tip pic zzzzz9.99.
+               10 col 1 value 'GRAND TOTAL (INC. TIP):'.
+               10 col 24 source f-total-with-tip pic zzzzz9.99.
 
        01 type is report footing.
            05 line number plus 1.
-               10 col 1 source c-stars pic x(38).
+               10 col 1 source c-stars pic x(32).
            05 line number plus 1.
-               10 col 14 value 'END REPORT'.
+               10 col 12 value 'END REPORT'.
            05 line number plus 1.
-               10 col 1 source c-stars pic x(38).
+               10 col 1 source c-stars pic x(32).
 
 
       *>=============================================================
@@ -136,6 +141,9 @@
            .
 
            accept ls-date from date yyyymmdd
+           accept ls-time from time
+           string ls-time(1:2) ":" ls-time(3:4) into LS-FORMATTED-TIME
+           end-string
 
           *> Copy the fields to the report file data items.
            compute f-amount-with-tax rounded = in-amount-with-tax
